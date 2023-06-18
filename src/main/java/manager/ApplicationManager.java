a5c7feb2dea1528a2e4fe9d9cb9638e4f40d1b0f
Yurii Shvydak <35421423+shvydak@users.noreply.github.com>
2023-06-18 20:55:58 +0300

    Update ApplicationManager.java

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -8,14 +8,22 @@ import org.testng.annotations.BeforeSuite;
 
 public class ApplicationManager {
     WebDriver webDriver;
-
+    
+    UserHelper userHelper;
 
     public void init() {
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         webDriver.navigate().to("https://");
+        
+        userHelper = new UserHelper(webDriver);
     }
 
+        public UserHelper getUserHelper() {
+        return userHelper;
+    }
 
     public void stop() {
     webDriver.close();
