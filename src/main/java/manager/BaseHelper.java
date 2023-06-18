d71cc9de81b31efec8aabea05d21f5303dbd3ad8
Yurii Shvydak <35421423+shvydak@users.noreply.github.com>
2023-06-18 20:56:39 +0300

    Update BaseHelper.java

--- src/main/java/manager/BaseHelper.java
+++ src/main/java/manager/BaseHelper.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class BaseHelper {
     WebDriver webDriver;
     public BaseHelper(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this); // Class initialisation
         this.webDriver = webDriver;
     }
 }
